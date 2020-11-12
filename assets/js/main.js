7ba978f3ad32db31793399291e6b77054c41f1f9
niccolosantelli <niccolo_santelli@live.it>
2020-11-12 15:36:42 +0100

    qualche modifica

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,8 +1,17 @@
 // document.getElementById('funz').innerHTML = "hello"
 
-var name = prompt("Qual'è il tuo nome?")
-var cognome = prompt("Qual'è il tuo cognome?")
-var color = prompt("Qual'è il tuo colore preferito?")
-var number = 19
+alert("Benvenuto in questa pagina");
+var name = prompt("Qual'è il tuo nome?");
+var cognome = prompt("Qual'è il tuo cognome?");
+var color = prompt("Qual'è il tuo colore preferito?");
+var number = 19;
 
-document.getElementById('funz').innerHTML = name + cognome + color + number
+document.getElementById('funz').innerHTML = "ciao, questa è la tua password: " + name + cognome + color + number ;
+
+var age = parseInt(prompt("quanti anni hai?"));
+var yeartoday = 2020;
+var by = yeartoday - age;
+
+document.getElementById('nascita').innerHTML = "e questa è la tua data di nascita: " + by ;
+
+document.getElementById('thanks').innerHTML = "Grazie per averci scelto! Buona giornata!"
