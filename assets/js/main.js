dfba1ed1df71109cc9e0f77cdffb811d7d1a88b5
niccolosantelli <niccolo_santelli@live.it>
2020-11-12 14:56:21 +0100

    completamento esercizio

---
+++ assets/js/main.js
@@ -0,0 +1,8 @@
+// document.getElementById('funz').innerHTML = "hello"
+
+var name = prompt("Qual'è il tuo nome?")
+var cognome = prompt("Qual'è il tuo cognome?")
+var color = prompt("Qual'è il tuo colore preferito?")
+var number = 19
+
+document.getElementById('funz').innerHTML = name + cognome + color + number
